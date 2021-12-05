1f4fd9e478ac279e473cf4df61d713ab53b6b292
Euna Ahn <eunaahn2001@icloud.com>
2021-12-05 23:24:04 +0900

    sookmyung 암호 페이지 추가

---
+++ app/src/main/java/com/example/sookmyungchat/EnterCode.java
@@ -0,0 +1,42 @@
+package com.example.sookmyungchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EnterCode extends AppCompatActivity {
+
+    private EditText editcode;
+    private String real_code;
+    private Button codebutton;
+    private String code;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_code);
+
+        real_code = getString(R.string.enter_code);
+
+
+        codebutton = findViewById(R.id.codebutton);
+        codebutton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editcode = findViewById(R.id.editText);
+                code = editcode.getText().toString();
+                if(code.equals(real_code)) {
+                    Intent intent = new Intent(getApplication(), MainActivity.class);
+                    startActivity(intent);
+                } else{
+
+                }
+            }
+        });
+    }
+}
