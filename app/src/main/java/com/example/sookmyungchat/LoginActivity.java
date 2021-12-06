5e3c618eefb8c7536378ddf8853df1111473638a
Euna Ahn <eunaahn2001@icloud.com>
2021-12-06 10:13:09 +0900

    Update LoginActivity.java

--- app/src/main/java/com/example/sookmyungchat/LoginActivity.java
+++ app/src/main/java/com/example/sookmyungchat/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -35,8 +36,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        signInButton = findViewById(R.id.SignIn_Button);
-
         // [START config_signin]
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -46,21 +45,18 @@ public class LoginActivity extends AppCompatActivity {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         // [END config_signin]
 
-        // [START initialize_auth]
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-        if(mAuth.getCurrentUser()!= null) {
-            Intent intent = new Intent(getApplication(), EnterCode.class);
-            startActivity(intent);
-            //finish();
-        }
-        // [END initialize_auth]
-
-        signInButton.setOnClickListener(new View.OnClickListener() {
+        signInButton = findViewById(R.id.SignIn_Button);
+        signInButton.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                signIn();
-
+            public void onClick(View view) {
+                if(mAuth.getCurrentUser()!= null) {
+                    signIn();
+                    Intent intent = new Intent(getApplication(), EnterCode.class);
+                    startActivity(intent);
+                } else{
+                    Toast.makeText(LoginActivity.this, "계정을 다시 선택해주세요!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
